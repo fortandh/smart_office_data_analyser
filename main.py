b87ecbda6b6344879538f86e83ed8bec930cff5d
fortandh <2217496259@qq.com>
2019-12-16 12:55:16 +0800

    Rename the result file name.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 import re
 
 folder_path = r"E:\work\1238257272382\results6"
-aim_file_name = "xx.csv"
 group_size = 10
+iteration_num = 100
 
 if __name__ == '__main__':
     # result6
@@ -15,9 +15,10 @@ if __name__ == '__main__':
         for config_name in os.listdir(folder_path):
             config_path = r"{}\{}".format(folder_path, config_name)
             if os.path.isdir(config_path):
+                aim_file_name = r"{}.csv".format(config_name)
                 aim_file_path = r"{}\{}".format(config_path, aim_file_name)
                 with open(aim_file_path, "w") as aim_file:
-                    result_average_list = [0.0]*100
+                    result_average_list = [0.0]*iteration_num
                     for data_file_name in os.listdir(config_path):
                         if data_file_name != aim_file_name:
                             data_file_path = r"{}\{}".format(config_path, data_file_name)
@@ -31,9 +32,9 @@ if __name__ == '__main__':
                                         # result_average_list[int(result[0])] += float(result[1])
                                         result_average_list[j] += float(result[1])
                                         j += 1
-                    for i in list(range(100)):
+                    for i in list(range(iteration_num)):
                         result_average_list[i] /= group_size
-                    for i in list(range(100)):
+                    for i in list(range(iteration_num)):
                         aim_file.write("{},{}\n".format(i, result_average_list[i]))
     else:
         print("文件夹不存在！\n")
